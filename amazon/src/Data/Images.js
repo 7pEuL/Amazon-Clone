9798e95999a3ef264c0c621d455ff0289abf16d5
= <keyoel7@gmail.com>
2023-01-24 18:50:35 -0500

    Data added

---
+++ amazon/src/Data/Images.js
@@ -0,0 +1,17 @@
+import USAFlag from './Images/usa-flag.jpg'
+import Cart from './Images/amazonCart.png'
+
+const AmazonLogo = 'https://pngimg.com/uploads/amazon/amazon_PNG11.png'
+const AmazonLogoWifi = 'https://m.media-amazon.com/images/I/111ORq5JUKL.jpg'
+
+const AmazonBanner = 'https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg'
+
+const images = {
+    AmazonLogo: AmazonLogo,
+    AmazonLogoWifi: AmazonLogoWifi,
+    AmazonBanner: AmazonBanner,
+    USAFlag: USAFlag,
+    Cart: Cart,
+}
+
+export default images
